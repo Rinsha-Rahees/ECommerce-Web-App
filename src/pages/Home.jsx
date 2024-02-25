8eee95ee3939d3c5ffc36489ed81911007850bc8
RinshaRahees <104898673+RinshaRahees@users.noreply.github.com>
2024-02-25 20:53:34 +0530

    Add: React route for home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Slides from "../Components/HeroSectionUI/Slides";
+import ProductList from "../Components/ProductSectionUI/ProductList";
+import AdvertisementBar from "../Components/AdvertisementBar";
+import NavBarUI from "../Components/NavigationBarUI/NavBarUI";
+import TopBar from "../Components/TopBar";
+import Footer from "../Components/FooterUI/Footer";
+import CategoryBar from "../Components/CategoryBarUI/CategoryBar";
+
+function Home() {
+  return (
+    <>
+      <TopBar />
+      <NavBarUI />
+      <CategoryBar />
+
+      <Slides />
+      <AdvertisementBar />
+      <ProductList />
+
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
